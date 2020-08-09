91ead0000d9817d02698ec16c93ec78f50cd0c87
Elizaveta Nafanailidi <liza-nafanailidi@yandex.ru>
2020-08-09 15:04:53 +0200

    first commit

---
+++ homeworks/homework_1/task_1.py
@@ -0,0 +1,14 @@
+""" 1. Поработайте с переменными, создайте несколько, выведите на экран, запросите
+у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+"""
+
+name1 = "Liza"
+age1 = 20
+
+print("Hello. My name is", name1, ". I am", age1, "years old.")
+
+
+name2 = input("Please, enter your name...   ")
+age2 = input("Please, enter your age...   ")
+
+print("You are", name2, "and you are", age2, "years old.\nNice to meet you!")
---
+++ homeworks/les1.py
@@ -0,0 +1,15 @@
+
+name = input("Enter your name ")
+
+print(name)
+
+var_str1 = 'hello, my friend\n'
+var_str2 = "hello, my frined"
+var_str3 = """hello,
+my
+friend"""
+
+
+print(1, var_str1)
+print(2, var_str2)
+print(3, var_str3)
